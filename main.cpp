4b793f69aea80dce6e11955e8a39ae01853afd81
Victor Jimenez <victor.jimenez1@udea.edu.co>
2020-10-09 13:10:33 -0500

    Se organiza

--- main.cpp
+++ main.cpp
@@ -2,41 +2,22 @@
 using namespace std;
 int main()
 {
-    int matriz[50][50],dim, suma=0,suma1=0;
-    cout<<"Ingrese la dimension: "; cin>>dim;
-    for(int i=0;i<dim;i++){
-        for(int j=0;j<dim;j++){
-            cout<<"Ingrese un numero ["<<i<<"]["<<j<<"]: "; cin>>matriz[i][j];
-        }
-    }
-    for(int i=0;i<dim;i++){
-        suma1=0;
-        int suma2=0, suma3=0, suma4=0;
-        for(int j=0;j<dim;j++){
-            if(i==0)
-                suma+=matriz[i][j];
-         else{
-           suma1+=matriz[i][j]; suma2+=matriz[j][i]; suma3+=matriz[j][j]; suma4+=matriz[j][dim-j-1];
-            }
-        }
-          if((suma!=suma1)&&i>0){
-              cout<<"No es magico"<<endl;
-          break;
-          }
-          else if((suma!=suma2)&&i>0){
-              cout<<"No es magico"<<endl;
-          break;
-          }
-          else if((suma!=suma3)&&i>0){
-              cout<<"No es magico"<<endl;
-          break;
-          }
-          else if((suma!=suma4)&&i>0){
-              cout<<"No es magico"<<endl;
-          break;
-          }
-    }
-    if(suma==suma1)
-        cout<<"El cuadrado es magico"<<endl;
+    int x,y,ancho,alto;
+    int rect1[4]={}, rect2[4]={};
+    do{
+    cout<<"Ingrese la coordenada x: ";cin>>x;
+    cout<<"Ingrese la coordenada y: ";cin>>y;
+    cout<<"Ingrese el ancho: ";cin>>ancho;
+    cout<<"Ingrese el lato: ";cin>>alto;
+    }while(ancho<=0||alto);
+    rect1[0]=x; rect1[1]=y; rect1[2]=ancho; rect1[3]=alto;
+    do{
+    cout<<"Ingrese la coordenada x: ";cin>>x;
+    cout<<"Ingrese la coordenada y: ";cin>>y;
+    cout<<"Ingrese el ancho: ";cin>>ancho;
+    cout<<"Ingrese el lato: ";cin>>alto;
+    }while(ancho<=0||alto);
+    rect2[0]=x; rect2[1]=y; rect2[2]=ancho; rect2[3]=alto;
+
     return 0;
 }
